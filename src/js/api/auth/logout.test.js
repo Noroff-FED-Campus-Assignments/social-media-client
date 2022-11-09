3f9f2d3e64d5f67bf6e348219d2a3a2da029adf6
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-09 21:26:05 +0100

    test jest

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -2,6 +2,5 @@ import { logout } from './logout.js';
 
 test('removes accesstoken on logout', async () => {
     await logout();
-    const token = localStorage.getItem('token');
-    expect(token).toBeNull();
+    expect(localStorage.getItem('token')).toBeNull();
 });
